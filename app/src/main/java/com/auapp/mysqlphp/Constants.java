dece8a378c796dece0ce06c12a01ebea6ac57894
Abhijeet Shinde13 <33342683+abhijeet-shinde264@users.noreply.github.com>
2020-06-02 19:12:25 +0530

    Initial commit

---
+++ app/src/main/java/com/auapp/mysqlphp/Constants.java
@@ -0,0 +1,6 @@
+package com.auapp.mysqlphp;
+
+class Constants {
+    private static final  String ROOT_URL = "http://192.168.0.7/Android/v1/";
+    public static final String URL_REGISTER = "http://192.168.0.7/Android/v1/registerUser.php";
+}
